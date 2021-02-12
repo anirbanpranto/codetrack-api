7a5f6e77694ce437210f31c56631c80c7660e435
anirbanpranto <anirbanpranto@gmail.com>
2021-02-13 01:50:12 +0800

    created schema for user and problems

---
+++ README.md
@@ -0,0 +1,10 @@
+# CodeTrack: API
+
+An app for keeping track of algorithm problems that you have solved online. Originally I used to use a spreadsheet to keep track of my progress. But I decided to make an app because why not xD
+
+This is the restful API repo. There will be a mobile repo and a web repo.
+### Tech stack:
+* Nodejs
+* Express
+* Swagger
+* MongoDB
---
+++ src/models/problem.model.js
@@ -0,0 +1,89 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const { toJSON, paginate } = require('./plugins');
+const { roles } = require('../config/roles');
+
+const problemSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    round: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    number: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    number: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    creator_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      unique: false,
+    },
+    status: {
+      type: Array,
+      required: false,
+      trim: true,
+      default: 'On Progress',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+userSchema.plugin(toJSON);
+userSchema.plugin(paginate);
+
+/**
+ * Check if email is taken
+ * @param {string} email - The user's email
+ * @param {ObjectId} [excludeUserId] - The id of the user to be excluded
+ * @returns {Promise<boolean>}
+ */
+userSchema.statics.isEmailTaken = async function (email, excludeUserId) {
+  const user = await this.findOne({ email, _id: { $ne: excludeUserId } });
+  return !!user;
+};
+
+/**
+ * Check if password matches the user's password
+ * @param {string} password
+ * @returns {Promise<boolean>}
+ */
+userSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
+
+/**
+ * @typedef User
+ */
+const Problem = mongoose.model('Problem', problemSchema);
+
+module.exports = Problem;
